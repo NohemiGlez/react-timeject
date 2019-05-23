5a4821bd5207cf7f82f1640473b4b4a9456eca87
omaru <omarux@gmail.com>
2019-05-22 23:27:14 -0600

    project mockup

--- src/component/Project.js
+++ src/component/Project.js
@@ -3,13 +3,13 @@ import React from 'react'
 const Project = ({ projects }) => {
       return (
         <div>
-          <center><h1>Project List</h1></center>
+          <center><h1>Projects </h1></center>
           {projects.map((project) => (
             <div class="card">
               <div class="card-body">
                 <h5 class="card-title">{project.name}</h5>  
                 <p class="card-text">{project.description}</p>
-                <a href="#" class="card-link">View Project</a>
+                <a href="#" class="card-link">View {project.name} Project</a>
               </div>
               <div class="progress">
                 <div class="progress-bar" role="progressbar" aria-valuenow="{project.progress}" aria-valuemin="0" aria-valuemax="100"></div>
